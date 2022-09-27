4e83ab4505c51e05a02fb4d9bf0d996887875576
Dmitry Krepak <76948301+dimakrepak@users.noreply.github.com>
2022-09-27 21:29:30 +0300

    update

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple React Modal
+
+import { Modal } from 'modal-react';
+
+# Props
+
+- `children` - The modal content.
+- `isVisible` - Shows the modal.
+- `backdropColor ` - Backdrop/mask color.
+- `onBackdropClick` - Event is called on backdrop/mask click.
--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import './App.css';
 
 import { useState } from 'react';
 
-import reactLogo from './assets/react.svg';
-import { Modal } from './Modal/Modal';
+import { Modal } from './Modal';
 
 function App() {
   const [modalIsOpen, setModalIsOpen] = useState(true);
@@ -12,18 +11,7 @@ function App() {
     <>
       <div className="dd" onClick={() => setModalIsOpen(true)}></div>
       <Modal isVisible={modalIsOpen} onBackdropClick={() => setModalIsOpen(false)}>
-        <div className="check">
-          <div>
-            <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-              <img src="/vite.svg" className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          </div>
-          <h1>Vite + React</h1>
-          <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </div>
+        <div className="check">Modal</div>
       </Modal>
     </>
   );
